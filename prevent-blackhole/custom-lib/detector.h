7ce518f21f414832d2d306608b9c2d4d6fcee2fb
longth57 <84077194+kirazero17@users.noreply.github.com>
2026-04-20 13:57:56 +0700

    thêm file chuẩn bị cho chống blackhole

---
+++ prevent-blackhole/custom-lib/detector.h
@@ -0,0 +1,28 @@
+#ifndef DETECTOR_H
+#define DETECTOR_H
+
+#include "contiki.h"
+#include "net/routing/rpl-lite/rpl.h"
+#include "net/ipv6/uip.h"
+#include "net/ipv6/uip-icmp6.h"
+#include "net/netstack.h"
+
+/*---------------------------------------------------------------------------*/
+static enum netstack_ip_action
+ip_input(void)
+{
+    return NETSTACK_IP_PROCESS;
+}
+/*---------------------------------------------------------------------------*/
+static enum netstack_ip_action
+ip_output(const linkaddr_t *localdest)
+{
+    return NETSTACK_IP_PROCESS;
+}
+/*---------------------------------------------------------------------------*/
+struct netstack_ip_packet_processor packet_processor = {
+    .process_input = ip_input,
+    .process_output = ip_output};
+/*---------------------------------------------------------------------------*/
+
+#endif /* DETECTOR_H */
